c5b0cf9757dd1753198b3b44d7d24f052b72cb1a
AleQRQ <aleqrq@gmail.com>
2022-06-22 09:13:10 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import DxButton from 'devextreme-vue/button';
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+const count = ref(0);
+</script>
+
+<template>
+  <p>count: {{count}}</p>
+  <DxButton
+    text="Click me"
+    @click="count++"
+  />
+  <img alt="Vue logo"  src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
